c034993537999e01fca8a61d0ec2dfb22857a8a6
atarashiiJS <nduytan.uit.a3@gmail.com>
2023-05-08 14:58:22 +0700

    feat: implement user

---
+++ HueFestivalTicket/Models/User/User.cs
@@ -0,0 +1,10 @@
+﻿namespace HueFestivalTicket.Models.User
+{
+    public class User
+    {
+        public String Username { get; set; } = String.Empty;
+
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
+    }
+}
---
+++ HueFestivalTicket/Models/User/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HueFestivalTicket.Models.User
+{
+    public class UserDto
+    {
+        [Required]
+        public String Username { get; set; } = String.Empty;
+        public String Password { get; set; } = String.Empty;
+
+    }
+}
