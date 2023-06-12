42b018600675867fa205f51d80e66fe7cb448524
atarashiiJS <nduytan.uit.a3@gmail.com>
2023-06-12 10:31:10 +0700

    fix: fixed qrcode

---
+++ HueFestivalTicket/Controllers/QrcodeController.cs
@@ -0,0 +1,41 @@
+﻿using HueFestivalTicket.Database;
+using HueFestivalTicket.Models.ProgramFes;
+using HueFestivalTicket.Models.qrCode;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+using System.IO;
+using QRCoder;
+
+namespace HueFestivalTicket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrcodeController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public QrcodeController(DataContext context)
+        {
+            _context = context;
+
+
+        }
+
+        //[HttpPost]
+        ////[HttpPost, Authorize(Roles = "Admin")]
+        //public string ReadQRCode(string imagePath)
+        //{
+        //    using (var bitmap = new Bitmap(imagePath))
+        //    {
+        //        var qrCodeBitmap = new QRCodeBitmap(bitmap);
+        //        var qrCodeDecoder = new QRCodeDecoder();
+        //        var qrCodeText = qrCodeDecoder.Decode(qrCodeBitmap);
+
+        //        return qrCodeText;
+        //    }
+        //}
+
+
+    }
+}
---
+++ HueFestivalTicket/Models/qrCode/Qrcode.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HueFestivalTicket.Models.qrCode
+{
+    public class Qrcode
+    {
+        [Required]
+        public string qrcode { get; set; }
+    }
+}
